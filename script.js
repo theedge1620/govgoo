e7e939be57f9c2227df4a2d1acadb1516b2f13c9
Zooropa2023 <jason.carneal@nrc.gov>
2023-07-18 20:53:47 -0400

    adding error message handling in JS file

--- script.js
+++ script.js
@@ -53,48 +53,61 @@ button.addEventListener('click', async ()=> {
             
 
             table.setAttribute("class", "jobs_table");
+
+            if (jobsArray.length > 0) {
    
-            for(var i = 0; i < jobsArray.length; i++) {
-                var row = table.insertRow(i);
+                for(var i = 0; i < jobsArray.length; i++) {
+                    var row = table.insertRow(i);
 
-                var payType = jobsArray[i].MatchedObjectDescriptor.PositionRemuneration[0].Description;
+                    var payType = jobsArray[i].MatchedObjectDescriptor.PositionRemuneration[0].Description;
 
-                
-
-                if (payType == 'Per Year') {  // if this is an annual salary, fix the string returned.
                     
-                    var payString = jobsArray[i].MatchedObjectDescriptor.PositionRemuneration[0].MaximumRange;
-                    var payNumber = Number(payString);
-                    var fixedString = "$"  +payString.slice(0,payString.length-5) + "," + payString.slice(payString.length-5,payString.length-2)+ ".00" + " / year";
 
-                    // check if the job pays more annually than the user input:
+                    if (payType == 'Per Year') {  // if this is an annual salary, fix the string returned.
+                        
+                        var payString = jobsArray[i].MatchedObjectDescriptor.PositionRemuneration[0].MaximumRange;
+                        var payNumber = Number(payString);
+                        var fixedString = "$"  +payString.slice(0,payString.length-5) + "," + payString.slice(payString.length-5,payString.length-2)+ ".00" + " / year";
 
-                    if (payNumber > salaryDesired) {
-                        var outputFlag = true;
+                        // check if the job pays more annually than the user input:
+
+                        if (payNumber > salaryDesired) {
+                            var outputFlag = true;
+                        }
+                        else {
+                            var outputFlag = false;
+                        }
+
                     }
                     else {
-                        var outputFlag = false;
+                        var fixedString = "$"+jobsArray[i].MatchedObjectDescriptor.PositionRemuneration[0].MaximumRange + " / hour";
+                        var outputFlag = checkBox.checked;
+                    };
+
+
+
+                    
+                    closingDate = jobsArray[i].MatchedObjectDescriptor.ApplicationCloseDate;
+                    if (outputFlag) {  // output a row if the jobs match the job parameters:
+
+                        row.insertCell(0).innerHTML = jobsArray[i].MatchedObjectDescriptor.PositionTitle;
+                        row.insertCell(1).innerHTML = jobsArray[i].MatchedObjectDescriptor.DepartmentName;
+                        row.insertCell(2).innerHTML = fixedString;
+                        row.insertCell(3).innerHTML = closingDate.slice(0,10);
+                        row.insertCell(4).innerHTML = "<a href='"+ jobsArray[i].MatchedObjectDescriptor.PositionURI+"' target='_blank'> Apply to Job </a>";
+
                     }
-
                 }
-                else {
-                    var fixedString = "$"+jobsArray[i].MatchedObjectDescriptor.PositionRemuneration[0].MaximumRange + " / hour";
-                    var outputFlag = checkBox.checked;
-                };
+            }
+            else {
+                var row = table.insertRow(i);
+                row.insertCell(0).innerHTML = "No jobs returned for this search.";
+                row.insertCell(1).innerHTML = "";
+                row.insertCell(2).innerHTML = "";
+                row.insertCell(3).innerHTML = "";
+                row.insertCell(4).innerHTML = "";
 
 
-
-                
-                closingDate = jobsArray[i].MatchedObjectDescriptor.ApplicationCloseDate;
-                if (outputFlag) {  // output a row if the jobs match the job parameters:
-
-                    row.insertCell(0).innerHTML = jobsArray[i].MatchedObjectDescriptor.PositionTitle;
-                    row.insertCell(1).innerHTML = jobsArray[i].MatchedObjectDescriptor.DepartmentName;
-                    row.insertCell(2).innerHTML = fixedString;
-                    row.insertCell(3).innerHTML = closingDate.slice(0,10);
-                    row.insertCell(4).innerHTML = "<a href='"+ jobsArray[i].MatchedObjectDescriptor.PositionURI+"' target='_blank'> Apply to Job </a>";
-
-                }
             }
         
             var header = table.createTHead();
