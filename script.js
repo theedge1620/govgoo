5ca58ef488354d5b4ec14afcdd4dda108a88c227
Zooropa2023 <jason.carneal@nrc.gov>
2023-07-18 20:02:27 -0400

    adding updates to user interface and logic.

--- script.js
+++ script.js
@@ -12,7 +12,12 @@ var authKey = 's7pZZOfktyr/8azgdWdjvpLw4V6RY6W9Bl5wzr1FozY=';
 button.addEventListener('click', async ()=> {
 
     // Set up the URL query for remote jobs:
-    let jobKeyWord = document.querySelector("input").value;
+    let jobKeyWord = document.getElementById("keywordInput").value;
+    let salaryInput = document.getElementById("desiredSalary").value;
+    var salaryDesired = Number(salaryInput);
+    let checkBox = document.getElementById("partTimeHourly");
+
+
     let queryUrl = 'https://data.usajobs.gov/api/Search?RemoteIndicator=True&Keyword='+jobKeyWord+'&ResultsPerPage=250'
     
     // check if a table exists on the page already (for multiple searches):
@@ -40,20 +45,58 @@ button.addEventListener('click', async ()=> {
         var jobsArray = response.data.SearchResult.SearchResultItems;
         console.log(jobsArray.length);
 
-        /// create a Table with the returned job results from the query:
+        /// define the function to create a Table with the returned job results from the query:
 
         function createTable() {
 
-            var headers = ["Job Title", "Agency", "Link"];
+            var headers = ["Job Title", "Agency", "Max Pay","Closing Date","Link"];
             var table = document.createElement("TABLE");  //makes a table element for the page
+
+            
             
             table.setAttribute("class", "jobs_table");
    
             for(var i = 0; i < jobsArray.length; i++) {
                 var row = table.insertRow(i);
-                row.insertCell(0).innerHTML = jobsArray[i].MatchedObjectDescriptor.PositionTitle;
-                row.insertCell(1).innerHTML = jobsArray[i].MatchedObjectDescriptor.DepartmentName;
-                row.insertCell(2).innerHTML = "<a href='"+ jobsArray[i].MatchedObjectDescriptor.PositionURI+"' target='_blank'> Apply to Job </a>";
+
+                var payType = jobsArray[i].MatchedObjectDescriptor.PositionRemuneration[0].Description;
+
+                
+
+                if (payType == 'Per Year') {  // if this is an annual salary, fix the string returned.
+                    
+                    var payString = jobsArray[i].MatchedObjectDescriptor.PositionRemuneration[0].MaximumRange;
+                    var payNumber = Number(payString);
+                    var fixedString = "$"  +payString.slice(0,payString.length-5) + "," + payString.slice(payString.length-5,payString.length-2)+ ".00" + " / year";
+
+                    // check if the job pays more annually than the user input:
+
+                    if (payNumber > salaryDesired) {
+                        var outputFlag = true;
+                    }
+                    else {
+                        var outputFlag = false;
+                    }
+
+                }
+                else {
+                    var fixedString = "$"+jobsArray[i].MatchedObjectDescriptor.PositionRemuneration[0].MaximumRange + " / hour";
+                    var outputFlag = checkBox.checked;
+                };
+
+
+
+                
+                closingDate = jobsArray[i].MatchedObjectDescriptor.ApplicationCloseDate;
+                if (outputFlag) {  // output a row if the jobs match the job parameters:
+
+                    row.insertCell(0).innerHTML = jobsArray[i].MatchedObjectDescriptor.PositionTitle;
+                    row.insertCell(1).innerHTML = jobsArray[i].MatchedObjectDescriptor.DepartmentName;
+                    row.insertCell(2).innerHTML = fixedString;
+                    row.insertCell(3).innerHTML = closingDate.slice(0,10);
+                    row.insertCell(4).innerHTML = "<a href='"+ jobsArray[i].MatchedObjectDescriptor.PositionURI+"' target='_blank'> Apply to Job </a>";
+
+                }
             }
         
             var header = table.createTHead();
@@ -70,7 +113,7 @@ button.addEventListener('click', async ()=> {
         
         createTable();
        
-     })
+     });
             
      .catch(err => {
         console.log(err);
